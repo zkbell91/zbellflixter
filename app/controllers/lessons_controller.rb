8351913b80686aebb85787a1a3bc4e649ca25d02
Zachary Bell <z.bell@me.com>
2019-06-22 00:25:09 +0000

    Add Stripe integration for accepting payments

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,30 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
+
 
   def show
 
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course) 
+      redirect_to course_path(current_lesson.section.course), alert: 'Please Enroll in course before viewing lesson'
+    end
+  end
+
+  # def enrolled_in?(course)
+  #   enrolled_courses = []
+  #   enrollments.each do |enrollment|
+  #     enrolled_courses << enrollment.course
+  #   end
+  #   return enrolled_courses.include?(course)
+  # end
+
+  
 
   helper_method :current_lesson
   def current_lesson
