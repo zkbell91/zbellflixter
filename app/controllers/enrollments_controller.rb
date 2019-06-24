4dfcf27e4b4754516db348385944cbc0fd65912d
Zachary Bell <z.bell@me.com>
2019-06-24 22:02:23 +0000

    Add support for RankedModel gem

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,22 +2,23 @@ class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
- 
-    # Amount in cents
-    @amount = (current_course.cost * 100).to_i
+    if current_course.premium?
+      # Amount in cents
+      @amount = (current_course.cost * 100).to_i
 
-    customer = Stripe::Customer.create(
-      email: params[:stripeEmail],
-      source: params[:stripeToken]
-    )
+      customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+      )
 
-    charge = Stripe::Charge.create(
-      customer: customer.id,
-      amount: @amount,
-      description: 'Flixter Premium Course',
-      currency: 'usd'
-    )
-  
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Flixter Premium Course',
+        currency: 'usd'
+      )
+    end
+    
   current_user.enrollments.create(course: current_course)
   redirect_to course_path(current_course)
   rescue Stripe::CardError => e
